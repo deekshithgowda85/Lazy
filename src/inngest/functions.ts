63958a92c83a7d398468713d4e6c6da53408b3d0
deekshithgowda85 <152056807+deekshithgowda85@users.noreply.github.com>
2025-07-17 22:14:24 +0530

    E2B and Docker

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,18 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event ,step}) => {
+    const sandboxId=await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("lazy-nextjs-test-2");
+      return sandbox.sandboxId;
+    });
+
+
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert Next.js developer. You write readable, maintainable code with precision. You write simple Next.js and React snippets with no comment lines (only code).",
@@ -14,7 +22,11 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet: ${event.data.value}`
     );
     
-    console.log(`Summarized output: ${output}`);
-    return { output };
-  }
+    const sandboxUrl=await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+    return { output ,sandboxUrl};
+  },
 );

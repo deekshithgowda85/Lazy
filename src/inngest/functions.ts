3b1de0751daf29e71f9cbff179047f0273a6f7e5
deekshithgowda85 <152056807+deekshithgowda85@users.noreply.github.com>
2025-07-19 15:42:12 +0530

    Projects added

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -163,7 +163,8 @@ export const codeAgentFunction = inngest.createFunction(
   await step.run("save-result", async() =>{
     if(isError){
       return await prisma.message.create({
-        data:{
+        data :{
+          projectId:event.data.projectId,
           content: "Something went wrong. Please try again.",
           role:"ASSISTANT",
           type: "ERROR",
@@ -173,6 +174,7 @@ export const codeAgentFunction = inngest.createFunction(
 
     return await prisma.message.create({
       data:{
+        projectId:event.data.projectId,
         content : result.state.data.summary,
         role: "ASSISTANT",
         type: "RESULT",
